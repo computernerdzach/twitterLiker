818929be391941b9400c670b867f2512af1d5a0f
computernerdzach <wizteczach@gmail.com>
2022-11-04 12:54:09 -0700

    more descriptive code and type hinting

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from utilities import *
 logfile = open(f'twitter-like-log-{name}.log', 'a')
 
 # Build bot using preferred credentials from above
-client = tweepy.Client(token_bearer, api_key, api_secret, access_token, access_token_secret)
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+client = tweepy.Client(token_bearer, api_key, api_secret,
+                       access_token=access_token, access_token_secret=access_token_secret)
+auth = tweepy.OAuth1UserHandler(api_key, api_secret,
+                                access_token=access_token, access_token_secret=access_token_secret)
+api = tweepy.API(auth=auth)
 
 # build lists of followers and following to compare and follow back followers
 followers = api.get_followers()
@@ -15,17 +17,17 @@ following = api.get_friends()
 
 # heading for log and readout
 message = f"[BEGIN LOG] -- {name}'s log -- {right_now()}"
-report(message, logfile)
+report(message=message, logfile=logfile)
 
 # main loop
 main = True
 tweet_run = 1
 while main:
     # follow back all followers
-    follow_back(followers, following, logfile, client, api)
+    follow_back(followers=followers, following=following, logfile=logfile, client=client, api=api)
 
     # set log sub-heading
-    logfile.write(set_sub_heading(tweet_run))
+    logfile.write(set_sub_heading(tweet_run=tweet_run))
 
     # get 100 tweets matching hashtags, and returning desired data
     tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'],
@@ -33,8 +35,8 @@ while main:
                                          user_fields=['username'], max_results=100)
 
     # like the tweets and randomly select a few authors to follow
-    like_tweet_random_follow(tweets, tweet_run, logfile, client, api)
+    like_tweet_random_follow(tweets=tweets, tweet_run=tweet_run, logfile=logfile, client=client, api=api)
 
     # increment and initiate next run of 100 tweets
-    log_next_run(logfile)
+    log_next_run(logfile=logfile)
     tweet_run += 1
